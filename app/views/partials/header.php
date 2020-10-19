802734f4ccee0a2325064c86741a600377332943
Aleksander <adishnica@yahoo.com>
2020-10-19 23:19:45 +0200

    Added files and header

---
+++ app/views/partials/header.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+	<head>
+		<title>PHP Tarot game</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<header>
+			<nav>
+				<ul>
+					<li>
+						
+					</li>
+					<li>
+						Home
+					</li>
+					<li>
+						Login
+					</li>
+					<li>
+						Register
+					</li>
+				</ul>
+			</nav>
+		</header>
