50c0cc77c5a84a9703eb12f96650efaf496cd1b5
Aleksander <adishnica@yahoo.com>
2020-10-20 23:06:42 +0200

    Additions

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -1,6 +1,12 @@
 <?php
 	class Controller
 	{
+		protected $production = 0;
+
+		public function __construct(){
+			echo 'construct';
+		}
+
 		function main(){
 			echo 'Main Controller';
 		}
---
+++ app/views/index.php
@@ -0,0 +1,6 @@
+<?php
+	include 'partials/header.php';
+
+
+
+	include 'partials/footer.php';
