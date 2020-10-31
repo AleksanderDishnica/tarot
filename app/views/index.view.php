ca7c192d58bfb499a74b75b120d4abfdd9403a94
Aleksander <adishnica@yahoo.com>
2020-11-01 00:03:31 +0100

    Added signup

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -1,5 +1,8 @@
 <div>
-	<?php if($logged_in < 0){ ?>
-		<a href="./index.php?game=1" class="btn btn-success" alt="Play tarot game">Play the game</a>
+	<?php if($logged_in == true){ ?>
+		<a href="./routing.php?game=1" class="btn btn-success" alt="Play tarot game">Play the game</a>
+	<?php }else{ ?>
+		<a href="./login.php" class="btn btn-primary" alt="Tarot game login">Login to play the game</a>
+		<a href="./signup.php" class="btn btn-primary" alt="Tarot game login">Signup</a>
 	<?php } ?>
 </div>
