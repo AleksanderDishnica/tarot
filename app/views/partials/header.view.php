ca8fbd01463b79ee64e27025e4c3a89556df325f
Aleksander <adishnica@yahoo.com>
2020-10-31 02:21:18 +0100

    Fixed nav for bootstrap

--- app/views/partials/header.view.php
+++ app/views/partials/header.view.php
@@ -21,20 +21,17 @@
 		<div class="container">
 			<div class="root">
 				<header>
-					<nav>
-						<ul>
-							<li>
-								<img src="" alt="">
-							</li>
-							<li>
-								<a href="">Home</a>
-							</li>
-							<li>
-								<a href="./login">Login</a>
-							</li>
-							<li>
-								<a href="./register">Register</a>
-							</li>
-						</ul>
+					<nav class="navbar navbar-expand-lg navbar-light bg-light">
+						<a class="navbar-brand" href="#">Navbar</a>
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+						<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+							<div class="navbar-nav">
+								<a class="nav-item nav-link active" href="">Home <span class="sr-only">(current)</span></a>
+								<a class="nav-item nav-link" href="./login">Login</a>
+								<a class="nav-item nav-link" href="./register">Register</a>
+							</div>
+						</div>
 					</nav>
 				</header>
